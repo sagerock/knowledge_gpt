39384f025736ab0b7c587801aa5a0d1622b7a0f9
sagerock <sage@sagerock.com>
2023-07-12 20:41:36 -0400

    Update sidebar.py

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -6,7 +6,26 @@ import os
 
 load_dotenv()
 
-api_key_input = st.secrets["API_KEYS"]["OPENAI_API_KEY"]
 
-st.session_state["OPENAI_API_KEY"] = api_key_input
+def sidebar():
+    with st.sidebar:
+       
+        api_key_input = st.secrets["API_KEYS"]["OPENAI_API_KEY"]
 
+        st.session_state["OPENAI_API_KEY"] = api_key_input
+
+        st.markdown("---")
+        st.markdown("# About")
+        st.markdown(
+            "📖KnowledgeGPT allows you to ask questions about your "
+            "documents and get accurate answers with instant citations. "
+        )
+        st.markdown(
+            "This tool is a work in progress. "
+            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
+            "with your feedback and suggestions💡"
+        )
+        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+        st.markdown("---")
+
+        faq()
